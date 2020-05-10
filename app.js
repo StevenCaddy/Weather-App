049132100eda4fd3b26a8dac8b8cc2a5b0c93cfd
Steven Caddy <steven.caddie@gmail.com>
2020-05-10 17:17:51 -0500

    api key start

---
+++ app.js
@@ -0,0 +1,9 @@
+//Just starting to get api call figured out
+
+var city = "Chicago";
+
+var openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city + "&appid=b44a9a6a895355217088a32cf9991c9c";
+    
+
+console.log(openWeatherURL);
