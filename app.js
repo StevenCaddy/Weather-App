7a303e568002e7be39835d923d1aadd405fc1dfb
Steven Caddy <steven.caddie@gmail.com>
2020-05-11 14:46:47 -0500

    less error ajax call

--- app.js
+++ app.js
@@ -2,21 +2,21 @@
 $("#submit").on("click", function(event) {
         event.preventDefault();
         console.log("i clicked it");
+
+
+        //Just starting to get api call figured out
+        const key = "&appid=b44a9a6a895355217088a32cf9991c9c";
+        let city = $("#city");
+        let openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city + key;
+
+        $.ajax({
+                url: openWeatherURL,
+                method: "GET"
+        }).done(function(response) {
+             console.log(openWeatherURL);
+        });
+
+        console.log(openWeatherURL);
+        console.log(city);
 });
-
-//Just starting to get api call figured out
-const key = "&appid=b44a9a6a895355217088a32cf9991c9c";
-let city = $("#city");
-let openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city + key;
-
-$.ajax({
-        url: openWeatherURL,
-        method: "GET"
-}).done(function(response) {
-        alert("openWeatherAPI");
-        var results = response.data;        
-});
-
-console.log(openWeatherURL);
-console.log(city);
