dc95fcb25c3a49124a6d3bebce4317a893f9b065
Steven Caddy <steven.caddie@gmail.com>
2020-05-11 13:13:42 -0500

    search bar setup

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 //Just starting to get api call figured out
-
-var city = "Chicago";
-
-var openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city + "&appid=b44a9a6a895355217088a32cf9991c9c";
-    
+const key = "&appid=b44a9a6a895355217088a32cf9991c9c";
+let city = "Chicago";
+let openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city + key;
 
 console.log(openWeatherURL);
