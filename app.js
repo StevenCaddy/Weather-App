6c132b61a9a51bcae7b5be68e0d5c367fe88f463
Steven Caddy <steven.caddie@gmail.com>
2020-05-12 17:08:34 -0500

    imperial and metric units added

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const key = "&appid=b44a9a6a895355217088a32cf9991c9c";
 let city = document.getElementById("city").value;
 let openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const unitImp = "&units=imperial";
+const unitMet = "&units=metric";
 
+// function runs after pushing submit btn and adds the city to
 function searchCity(){
         city = document.getElementById('city').value;
-        openWeatherURL = openWeatherURL + city + key;
+        openWeatherURL = openWeatherURL + city + unitImp + key;
         console.log(openWeatherURL);
         openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 };
-
