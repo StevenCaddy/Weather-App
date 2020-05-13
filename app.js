a23227ac8b4cf8cc20284ac53d346a5ba1d91b1a
Steven Caddy <steven.caddie@gmail.com>
2020-05-13 13:16:44 -0500

    Units display on html slider change

--- app.js
+++ app.js
@@ -4,12 +4,31 @@ let city = document.getElementById("city").value;
 let openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 const unitImp = "&units=imperial";
 const unitMet = "&units=metric";
+let slider = document.getElementById("myRange");
+let output = document.getElementById('demo');
 
+output.innerHTML = slider.value;
+
+//display the units of slider?
+slider.oninput = function() {
+        if (this.value == 1){
+                output.innerHTML = "Imperial";
+        } else if (this.value == 2){
+                output.innerHTML = "Metric";
+        }
+};
 // function runs after pushing submit btn and adds the city to
 function searchCity(){
         city = document.getElementById('city').value;
-        openWeatherURL = openWeatherURL + city + unitImp + key;
-        console.log(openWeatherURL);
-        openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+        slider = document.getElementById('myRange').value;
+        console.log(slider.value);
+        if (slider.value = 1) {
+                openWeatherURL = openWeatherURL + city + unitImp + key;
+                console.log(openWeatherURL);
+        } else if (slider.value = 2) {
+                openWeatherURL = openWeatherURL + city + unitMet + key;
+                console.log(openWeatherURL);
+        }
 
+        openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 };
